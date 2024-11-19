e357a5a08e63e71de7abf8e0dcecd8347a1d2a5c
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2024-11-19 17:41:22 +0000

    conformance tests, passing settings, graph drawing

--- snakemake-report-plugin-wrroc/snakemake_report_plugin_wrroc/__init__.py
+++ snakemake-report-plugin-wrroc/snakemake_report_plugin_wrroc/__init__.py
@@ -2,7 +2,10 @@ from dataclasses import dataclass, field
 from typing import Optional
 
 import snakemake
+from snakemake.logging import logger
 import os
+from subprocess import run, CalledProcessError
+
 
 from snakemake_interface_common.exceptions import WorkflowError  # noqa: F401
 from snakemake_interface_report_plugins.reporter import ReporterBase
@@ -19,10 +22,16 @@ from rocrate.model import ContextEntity, Person
 # of None (or False) or anything else that makes sense in your case.
 @dataclass
 class ReportSettings(ReportSettingsBase):
-    myparam: Optional[int] = field(
+    """Additional settings for the RO-Crate reporter.
+       They will occur in the Snakemake CLI as --report-wrroc-<param-name>
+       Make sure that any further defined fields are Optional (or bool) and specify a default
+       value of None (or False) or else Snakemake will demand these settings even when the
+       reporter is not in use. Use the "required" flag below for required options.
+    """
+    exclude: Optional[str] = field(
         default=None,
         metadata={
-            "help": "Some help text",
+            "help": "Comma-separated list of extra files to exclude",
             # Optionally request that setting is also available for specification
             # via an environment variable. The variable will be named automatically as
             # SNAKEMAKE_REPORT_<reporter-name>_<param-name>, all upper case.
@@ -42,11 +51,20 @@ class ReportSettings(ReportSettingsBase):
         },
     )
 
+    force: bool = field(
+        default=False,
+        metadata={
+            "help": "Continue even if there are non-conformities",
+            "env_var": False,
+            "required": False,
+        }
+    )
+
 
 # Required:
 # Implementation of your reporter
 class Reporter(ReporterBase):
-    def __post_init__(self, excludelist = [".snakemake", ".git", ".github", ".test", ".gitignore"]):
+    def __post_init__(self, excludelist = (".snakemake", ".git", ".github", ".test", ".gitignore")):
         # initialize additional attributes
         # Do not overwrite the __init__ method as this is kept in control of the base
         # class in order to simplify the update process.
@@ -55,56 +73,239 @@ class Reporter(ReporterBase):
         # In particular, the settings of above ReportSettings class are accessible via
         # self.settings.
         self.outdir = "ro-crate_out_copymetadata"
-        self.excludelist = excludelist
+        self.excludelist = list(excludelist)
         self.excludelist.append(self.outdir)
+
+        # Add user settings
+        if self.settings.exclude:
+            self.excludelist.extend(self.settings.exclude.split(','))
+
+        self.conformance_force = self.settings.force
+
         # Load the existing Workflow RO-Crate
-        self.old_crate = ROCrate(source='./')
-        self.crate = ROCrate()
+        try:
+            self.old_crate = ROCrate(source='./', exclude=self.excludelist)
+        except ValueError:
+            self.old_crate = None
+
+        # Create a new crate for output
+        self.crate = ROCrate(exclude=self.excludelist)
+
+
+    def check_essential_files(self):
+        """Check for the presence of essential files.
+
+           We want to scan/report everything even if there is an error.
+
+           See https://docs.google.com/document/d/1KozjchVFqrctBGooRR-OWpifyI0TRuoSWIUUGIDWNyI/edit?tab=t.0
+        """
+        errors = []
+
+        # A license
+        if not os.path.exists("LICENSE.md") or not os.path.exists("LICENSE.txt"):
+            errors.append("No LICENSE.md or LICENSE.txt found.")
+
+        # TODO - do we need CODE_OF_CONDUCT.md and CONTRIBUTING.md too? I'm making these
+        # warnings just now.
+
+        # Evidence of Git repo. This is probably not the best way to check but we'd like to know
+        # the remote URL
+        if not os.path.exists(".git/config"):
+            if os.path.exists("../.git/config"):
+                # We are within a GIT repo but not at the top level, so:
+                if not os.path.exists("workflowhub.yml"):
+                    errors.append("Since your workflow is in a subdirectory of your GIT repo,"
+                                  " you must include a 'workflowhub.yml' file.")
+            else:
+                errors.append("No .git/config file found. Is the code under source control?")
+
+        # We need a README.md
+        if not os.path.exists("README.md"):
+            errors.append("Add a README.md file to introduce your workflow.")
+
+        # The main workflow should be called "workflow/Snakefile"
+        main_snakefile = os.path.relpath(self.dag.workflow.main_snakefile)
+        if main_snakefile != "workflow/Snakefile":
+            # FIXME - this is returning nonsense in Snakemake 8. Does the regular html
+            # reporter get a meaningful value? Nope.
+            errors.append("Your main Snakefile needs to be called 'workflow/Snakefile'."
+                          f" Please rename {main_snakefile}.")
+
+        # And we want a config.yaml file.
+        if not os.path.exists("config/config.yaml"):
+            if os.path.exists("config/config.json"):
+                # For anything that needs to be hand-edited and not read by JS, YAML >> JSON.
+                errors.append("Please convert your config/config.json file to YAML format using"
+                              " <insert suggested converter tool here>.")
+            else:
+                errors.append("Please supply a default/sample configuration file for the workflow"
+                              " under 'config/config.yaml'.")
+
+        # GitHub wants a "CITATION.cff"
+        # It looks like we should be able to pull the info from this into the metadata -
+        # see https://www.researchobject.org/ro-crate/specification/1.1/contextual-entities.html#publications-via-citation-property
+        # but I'm not sure how useful this is or where that code would live. Given that the
+        # cffconvert tool (and library) already supports CFF to schema.org conversion I'd imagine
+        # this is already done in other tools.
+        if not os.path.exists("CITATION.cff"):
+            errors.append("You must include a 'CITATION.cff' file. If you are not requesting a"
+                          " specific citation for use of the workflow, please <link instrux here"
+                          " for making a minimal CFF, or maybe create a template>")
+
+        return errors
+
+    def check_desirable_files(self):
+        """Things that *should* be in the submission but are not essential.
+        """
+        errors = []
+
+        # WorkflowHub says the tests should be under "tests" but Snakemake says they should
+        # be under ".tests". Can we be opinionated about it?
+        if not os.path.isdir("tests"):
+            if os.path.isdir(".tests"):
+                errors.append("You have a '.tests' directory. Please rename it as 'tests' or else"
+                              " make a symlink called 'tests'.")
+            else:
+                errors.append("Please add a 'tests' directory with 'unit' and 'integration'"
+                              " subdirectories.")
+        elif not(os.path.isdir("tests/integration") and os.path.isdir("tests/unit")):
+            errors.append("Please create 'unit' and 'integration' subdirectories under 'tests'.")
+
+
+        if not(os.path.exists("CODE_OF_CONDUCT.md") and os.path.exists("CONTRIBUTING.md")):
+            errors.append("Please add CODE_OF_CONDUCT.md and CONTRIBUTING.md files."
+                          " You may be happy copying the versions from <suggest something here>")
+
+        return errors
+
+    def conformance_check(self):
+        """Ensure that some expected files are found. The rocrate module does
+           not scan the files until the crate is exported, so we have to look for the
+           files here.
+        """
+        essential_problems = self.check_essential_files()
+        if essential_problems:
+            for prob in essential_problems:
+                logger.error(f"Conformance error: {prob}")
+            msg = f"Exiting due to {len(essential_problems)} conformance issues."
+            if self.conformance_force:
+                logger.warning(f"Continuing despite {len(essential_problems)} conformance issues.")
+            else:
+                raise RuntimeError(f"Exiting due to {len(essential_problems)} conformance issues.")
+
+        desirable_problems = self.check_desirable_files()
+        if desirable_problems:
+            for prob in desirable_problems:
+                logger.warning(f"Conformance warning: {prob}")
+            logger.warning(f"Continuing despite len(desirable_problems) warnings.")
+
+        # images/rulegraph.svg should be something we can auto-generate. self.dag has methods dot()
+        # and rule_dot() which can make the graph for us, but it still needs converting to SVG.
+        if not os.path.exists("image/rulegraph.svg"):
+
+            logger.warning("Auto generating 'image/rulegraph.svg'")
+            try:
+                os.makedirs("image", exist_ok=True)
+                with open("image/rulegraph.dot", "x") as dotfh:
+                    print(self.dag.rule_dot(), file=dotfh)
+            except FileExistsError:
+                # Never mind, use the one we have. Maybe the user edited it.
+                logger.info("Using existing 'image/rulegraph.dot'")
+
+            # For converting .dot to .svg I don't see a better way than calling the graphviz
+            # program directly.
+            try:
+                run(['dot', '-Tsvg', 'image/rulegraph.dot', '-o', 'image/rulegraph.svg'],
+                     check = True,
+                     capture_output = True,
+                     text = True)
+            except CalledProcessError as e:
+                logger.error(str(e.stderr).rstrip())
+                logger.error("The 'dot' program returned the above error attempting to convert the rulegraph.")
+            except FileNotFoundError as e:
+                logger.error(str(e))
+                logger.error("The 'dot' program was not found. Unable to auto-convert the rulegraph.")
 
     def render(self):
-        # Render the report, using attributes of the base class.
+        try:
+            self.try_render()
+        except Exception as e:
+            # Catch all exceptions and turn them into error messages.
+            logger.error(e)
+
+
+    def try_render(self):
+        """Generate the crate, using the ROCrate library.
+        """
+        logger.info(f"Excludelist: {self.excludelist}")
+
+        self.conformance_check()
+
+        crate = self.crate
+        old_crate = self.old_crate
 
         # copy across files and data structure.
         # NOTE: we have to use the add_dataset and add_file commands,
         #       *not* the plain add command (as for the contextual entity),
         #       or the library will not load the files contained within
         #       the dataset directories.
-        for entity in self.old_crate.data_entities:
+        for entity in old_crate.data_entities:
             if 'Dataset' in entity.type:
-                self.crate.add_dataset(source=entity.id, dest_path=entity.id, 
+                crate.add_dataset(source=entity.id, dest_path=entity.id, 
                                        properties=entity.properties())
             elif 'File' in entity.type:
-                self.crate.add_file(source=entity.id, dest_path=entity.id,
+                crate.add_file(source=entity.id, dest_path=entity.id,
                                     properties=entity.properties())
+
+            if entity.id == old_crate.mainEntity:
+                crate.mainEntity = entity
         
         # copy desired metadata from old crate
-        for entity in self.old_crate.contextual_entities:
-            self.crate.add(entity)
+        for entity in old_crate.contextual_entities:
+            crate.add(entity)
+
+        # copy existing metadata
+        if old_crate.mainEntity:
+            crate.mainEntity = old_crate.mainEntity
+        if old_crate.license:
+            crate.license = old_crate.license
+        if old_crate.isBasedOn:
+            crate.isBasedOn = old_crate.isBasedOn
+        if old_crate.name:
+            crate.name = old_crate.name
+
+        # check that a workflow diagram is listed, and add if not
+        if 'image' not in crate.mainEntity.properties():
+            graph = crate.add_file(source='image/rulegraph.svg', dest_path='image/rulegraph.svg', 
+                               properties={'@type':['File','ImageObject']})
+            crate.mainEntity.append_to('image',graph)
 
 
         # Provenance Crate - add snakemake version
-        for entity in self.crate.contextual_entities:
+        for entity in crate.contextual_entities:
             if entity.type == 'ComputerLanguage' and 'snakemake' in entity.id.lower():
                 entity['version'] = snakemake.__version__.split("+")[0]
         
         # Provenance Crate - record execution of workflow as a CreateAction object
         instruments = {}
-        for entity in self.crate.data_entities:
+        for entity in crate.data_entities:
             if 'ComputationalWorkflow' in entity.type:
                 instruments["@id"] = entity.id
         workflow_run_properties = {
-            #"@id":"FIXME-a",
+            #"@id":"FIXME-add-workflow-run-properties-id",
             "@type":"CreateAction",
             "name":"Snakemake workflow run (FIXME)",
             "endTime":"FIXME date",
-            "instrument":instruments,
+            #"instrument":instruments,
             #"subjectOf":{"@id":"FIXME creative work (workflow?)"},
             "object":["FIXME inputs"],
             "result":["FIXME outputs"]
         }
-        print(workflow_run_properties)
-        workflow_run = self.crate.add(
-            ContextEntity(self.crate, properties=workflow_run_properties)
+        if '@id' in instruments:
+            workflow_run_properties['instruments'] = instruments
+        logger.info(workflow_run_properties)
+        workflow_run = crate.add(
+            ContextEntity(crate, properties=workflow_run_properties)
         )
 
         # Provenance Run Crate (individual step information)
@@ -125,14 +326,14 @@ class Reporter(ReporterBase):
             "familyName": "FIXME",
             "affiliation": "FIXME"
         }
-        agent = self.crate.add(Person(self.crate,
-                                      "FIXME-ORCID?",
-                                      properties=person_properties))
+        agent = crate.add(Person(crate,
+                                "FIXME-ORCID?",
+                                properties=person_properties))
         workflow_run.append_to( "agent", [agent] )
         
         
         # Reference CreateAction in the root Dataset
-        self.crate.root_dataset.append_to(
+        crate.root_dataset.append_to(
             "mentions" , [{"@id": workflow_run.id}]
         )
         
@@ -145,5 +346,6 @@ class Reporter(ReporterBase):
                 ]
 
 
-        self.crate.write(self.outdir)
+        crate.write(self.outdir)
+        crate.write_zip(self.outdir + ".zip")
 
